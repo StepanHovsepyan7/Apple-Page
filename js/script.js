f5c6a60d4e8cedb30e90b810e0f85be93b6587dd
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2025-01-30 16:56:53 +0400

    the project is finished

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 let menuA = document.querySelector('.menuA');
 let nav  = document.querySelector('.nav');
 let fabars = document.getElementsByClassName('fa-bars')[0];
+let menuLink = document.querySelectorAll('.menu-link'); 
 
 
 menuA.addEventListener('click',()=>{
@@ -15,3 +16,17 @@ menuA.addEventListener('click',()=>{
     }
 })
 
+
+menuLink.forEach((e)=>{
+    e.addEventListener('click',()=>{
+        nav.classList.remove('menuOpen');
+        if(fabars.classList.contains('fa-x')){
+            fabars.classList.remove('fa-x');
+            fabars.classList.add('fa-bars');
+        }
+    })
+})
+
+AOS.init({
+    once: false,
+})
